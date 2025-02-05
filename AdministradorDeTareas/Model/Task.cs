4078f6c5136f15d5d7989c1ecc1f689c69fb6d17
alexesp <aleizaes@hotmail.com>
2025-02-05 17:59:59 +0100

    Agregar archivos de proyecto.

---
+++ AdministradorDeTareas/Model/Task.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdministradorDeTareas.Model
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime StartDate { get; set; }
+        public bool IsComplete { get; set; }
+        public TimeSpan Timer { get; set; }
+
+        public TaskState TaskState { get; set; }
+        public TaskImportance TaskImportance { get; set; }
+        public TaskCategory TaskCategory { get; set; }
+    }
+
+    public enum TaskState
+    {
+        InProgress,
+        Complete,
+        NotStrarted,
+        Late,
+        Archived,
+        Deleted
+    }
+
+    public enum TaskCategory
+    {
+        Work,
+        Personal,
+        Home,
+        HelthWebeing,
+        Finance,
+        Shopping,
+        SocialFamily,
+        Education,
+        Travel,
+        Errands,
+        HobbiesLeisure,
+        VolunteeringComminity,
+        BirthdaysAnniversaries,
+        Projects,
+        LongTermGoals
+    }
+
+    public enum TaskImportance
+    {
+        Low,
+        Medium,
+        High,
+        Critical
+    }
+}
